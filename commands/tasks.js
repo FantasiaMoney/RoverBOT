ec9a07db74502ae7971af5ee6c98daa0e7e8a34b
RuslanMirov <ruslan777mirov@gmail.com>
2021-12-22 09:53:36 +0200

    Update

--- commands/tasks.js
+++ commands/tasks.js
@@ -1,6 +1,23 @@
-// Listen to /start command
+const config = require('../config.json')
+
 async function Tasks(BOT, message) {
-  await BOT.sendMessage(message.chat.id, "tasks")
+
+  config.TASKS.map(async (item, key) => {
+    const text = `<strong>
+    ✔️
+    ${item.description}
+    <a href="${item.link}">HERE</a>
+   </strong>
+   `
+
+   return await BOT.sendMessage(message.chat.id,
+     text
+     , {
+     parse_mode:'HTML',
+     disable_web_page_preview:true
+   })
+  }
+  )
 }
 
 module.exports = Tasks
